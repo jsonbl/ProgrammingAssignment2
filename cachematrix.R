829f6b63b5ba4b4adc4ad33ccb59e7d8ab03a071
jsonbl <jsonbl@users.noreply.github.com>
2017-05-20 13:33:56 -0500

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,47 @@
-## Put comments here that give an overall description of what your
-## functions do
+\## jsonbl
+## Coursera: Programing in R
+## Week 3 Programming Assignment 2
+## Caching the inverse of a matrix
 
-## Write a short comment describing this function
+## This function creates a special "matrix" object that can cache its inverse.
+## The function takes a single value that defines the dimensions of a square matrix filled with random values.
+## Optional second argument of TRUE/FALSE can be passed to decide if the inverse matrix is cached (default = TRUE) or not
+## usage example: myMatrix <- makeCacheMatrix(5, cache=TRUE)
+## output is a list that stores a vector, its matrix and its inverse matrix
+## ex: myMatrix$getv() returns the vector, myMatrix$getm() returns the matrix, and 
+##     myMatrix$getinv() returns the invers matrix
 
-makeCacheMatrix <- function(x = matrix()) {
-
+makeCacheMatrix <- function(x = matrix(), cache=TRUE) {
+  v <- sample(x*x)
+  m <- matrix(v,nrow=x,ncol=x)
+  getv <- function() v
+  getm <- function() m
+  if(cache == TRUE){
+    getinv <- function(x){
+      solve(m)
+    }
+    list(getv = getv, getm = getm, getinv = getinv)
+  }
+  else{
+    getinv <- function() inv <- NULL
+    list(getv = getv, getm = getm, getinv = getinv)
+  }
 }
 
+## This function retrieves the inverse matrix from the cache of the first matrix if available. 
+## If null, then the inverse matrix is calculated.
 
-## Write a short comment describing this function
-
-cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+cacheSolve <- function(x) {
+  inv <- x$getinv()
+  if(!is.null(inv)){
+    inv <- x$getinv()
+    message("getting cached data...")
+    return(inv)
+  }  
+  else{
+    message("cached data is not available, calculating...")
+    m <- x$getm()
+    inv <- solve(m)
+    return(inv)
+  }
 }
